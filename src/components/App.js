6df3a7476eb5ada5cf257c5614f110a4fbe0c840
Aman Raj <amanrajhenry@gmail.com>
2022-09-26 15:36:05 +0000

    Update 6 files

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,14 @@
 import React, { Component, useState } from "react";
-import '../styles/App.css';
+import "../styles/App.css";
 
 const App = () => {
   return (
     <div id="main">
-      < />
-      < />
-      < />
-      < />
+      <Heading />
+      <InputQuery />
+      <SubHeading />
+      <SubmitButton />
     </div>
-  )
-}
-
-
+  );
+};
+export default App;
